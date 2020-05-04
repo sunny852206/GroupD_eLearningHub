45690d81301bbb1ea84cb31124a6f28642c249db
vq37vhr <46409635+vq37vhr@users.noreply.github.com>
2020-05-03 21:16:02 -0700

    Update server.js
    API not running

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req, res) => res.send('API is not Running'));
 
 const PORT = process.env.PORT || 5000;
 
