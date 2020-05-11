4fdc74501d864eb5573d13bc5431c73243dfe9e6
vq37vhr <sunnylau1206@gmail.com>
2020-05-11 00:30:09 -0700

    Added front page (demo)

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Index from './Index';
 import UserProfile from './UserProfile';
 
 class Main extends Component {
   render() {
     return (
       <div>
+        <Index />
         <UserProfile />
       </div>
     );
--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -4,30 +4,30 @@ import '../App.css';
 class UserProfile extends Component {
   render() {
     return (
-      <div class='container'>
-        <div class='wrap'>
-          <div class='first'>
-            <div class='row'>
-              <div class='col-4 offset-md-1'>
-                <a class='profilePicture'>
+      <div className='container'>
+        <div className='wrap'>
+          <div className='userProfile'>
+            <div className='row'>
+              <div className='col-4 offset-md-1'>
+                <a className='profilePicture'>
                   <img
                     src='https://c-sf.smule.com/sf/s48/arr/73/57/77130078-659a-4900-a50b-18d4abe880ce.jpg'
                     alt=''
                   />
                 </a>
               </div>
-              <div class='col-4 offset-md-1'>
-                <div class='userInfo'>
-                  <div class='row'>
-                    <div class='col-12 text-center'>
+              <div className='col-4 offset-md-1'>
+                <div className='userInfo'>
+                  <div className='row'>
+                    <div className='col-12 text-center'>
                       <h1>Name</h1>
                     </div>
                   </div>
-                  <div class='row'>
-                    <div class='col-6'>
+                  <div className='row'>
+                    <div className='col-6'>
                       <h3>Home</h3>
                     </div>
-                    <div class='col-6'>
+                    <div className='col-6'>
                       <h3>Information</h3>
                     </div>
                   </div>
@@ -35,38 +35,38 @@ class UserProfile extends Component {
               </div>
             </div>
           </div>
-          <div class='second'>
-            <div class='row'>
-              <div class='col-4 text-center'>
-                <div class='UserProject1'>Project</div>
+          <div className='projectPanel'>
+            <div className='row'>
+              <div className='col-4 text-center'>
+                <div className='UserProject1'>Project</div>
               </div>
-              <div class='col-4 text-center'>
-                <div class='UserProject2'>Project</div>
+              <div className='col-4 text-center'>
+                <div className='UserProject2'>Project</div>
               </div>
-              <div class='col-4 text-center'>
-                <div class='UserProject3'>Project</div>
-              </div>
-            </div>
-            <div class='row'>
-              <div class='col-4 text-center'>
-                <div class='UserProject2'>Project</div>
-              </div>
-              <div class='col-4 text-center'>
-                <div class='UserProject1'>Project</div>
-              </div>
-              <div class='col-4 text-center'>
-                <div class='UserProject3'>Project</div>
+              <div className='col-4 text-center'>
+                <div className='UserProject3'>Project</div>
               </div>
             </div>
-            <div class='row'>
-              <div class='col-4 text-center'>
-                <div class='UserProject3'>Project</div>
+            <div className='row'>
+              <div className='col-4 text-center'>
+                <div className='UserProject2'>Project</div>
               </div>
-              <div class='col-4 text-center'>
-                <div class='UserProject2'>Project</div>
+              <div className='col-4 text-center'>
+                <div className='UserProject1'>Project</div>
               </div>
-              <div class='col-4 text-center'>
-                <div class='UserProject1'>Project</div>
+              <div className='col-4 text-center'>
+                <div className='UserProject3'>Project</div>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col-4 text-center'>
+                <div className='UserProject3'>Project</div>
+              </div>
+              <div className='col-4 text-center'>
+                <div className='UserProject2'>Project</div>
+              </div>
+              <div className='col-4 text-center'>
+                <div className='UserProject1'>Project</div>
               </div>
             </div>
           </div>
